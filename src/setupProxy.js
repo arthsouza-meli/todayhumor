722c2f5dcc4c10dd085e0978547cb9672b6ccda2
arthur souza <arthur.ghz@icloud.com>
2023-04-28 19:24:19 -0300

    chore: Ignore setupProxy.js file in Git

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-
-const apiProxy = createProxyMiddleware('/api', { target: 'http://localhost:8080' });
-
-module.exports = function(app) {
-    app.use(apiProxy);
-  }
-  
