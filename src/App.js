be0eb2c7c0fcb8d2d664ad24d719c220ca7ba604
arthur souza <arthur.ghz@icloud.com>
2023-04-28 19:29:34 -0300

    feat: footer

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<MoodForm />} />
         <Route path="/result" element={<MoodResult />} />
       </Routes>
-     {/* <Footer /> */}
+     <Footer />
     </div>
   );
 }
