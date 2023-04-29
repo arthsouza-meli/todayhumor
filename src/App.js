5d46a97f121d69c4adfb3cec32d9257104050030
arthur souza <arthur.ghz@icloud.com>
2023-04-29 20:45:24 -0300

    test redirect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import MoodForm from './components/MoodForm';
 import MoodResult from './components/MoodResult';
-import { analytics } from "./firebaseConfig";
-import { useEffect } from "react";
+
 
 function App() {
-  useEffect(() => {
-    analytics.logEvent("page_view");
-  }, []);
+
 
   return (
     <div className="container">
