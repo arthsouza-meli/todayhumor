8d2a4abebc91b487994ce9fe1f27ab8a2aa638c5
arthur souza <arthur.ghz@icloud.com>
2023-04-29 18:20:30 -0300

    firebase analytics

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-
-const apiProxy = createProxyMiddleware('/api', { target: 'https://api.worldmoodtoday.com' });
+const apiProxy = createProxyMiddleware('/api', { 
+  target: 'https://api.worldmoodtoday.com',
+  changeOrigin: true,
+  secure: true,
+  rejectUnauthorized: false,
+});
 
 module.exports = function(app) {
-    app.use(apiProxy);
-  }
-  
+  app.use(apiProxy);
+}
